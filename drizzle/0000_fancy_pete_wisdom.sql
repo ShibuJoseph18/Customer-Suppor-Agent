f6e8d6a2c79f3953ce2569a53daef1dc573cd410
shibujoseph <shibujoseph1815@gmail.com>
2026-03-11 10:37:21 +0530

    modify schema

--- drizzle/0000_fancy_pete_wisdom.sql
+++ drizzle/0000_fancy_pete_wisdom.sql
@@ -55,7 +55,9 @@ CREATE TABLE `order_lines` (
 CREATE TABLE `orders` (
 	`id` text PRIMARY KEY NOT NULL,
 	`user_id` text NOT NULL,
+	`status` text DEFAULT 'pending' NOT NULL,
 	`created_at` integer NOT NULL,
+	`updated_at` integer NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
