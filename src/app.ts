9dab68c669591109929a7a130b2d3738e121d542
shibujoseph <shibujoseph1815@gmail.com>
2026-03-11 10:45:45 +0530

    list all routes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
 import Koa from "koa";
+import bodyParser from "koa-bodyparser";
 import Router from "@koa/router";
+import { authRouter } from "./routes/auth.js";
+import { productsRouter } from "./routes/products.js";
+import { cartsRouter } from "./routes/carts.js";
+import { ordersRouter } from "./routes/orders.js";
+import { reviewsRouter } from "./routes/reviews.js";
+import { cartItemsRouter } from "./routes/cartItems.js";
+
 export const app = new Koa();
 
-const router = new Router();
+const rootRouter = new Router();
 
-router.get("/health", (ctx) => {
-  ctx.body = {
-    status: "ok",
-  }
-})
+rootRouter.get("/health", (ctx) => {
+  ctx.body = { status: "ok" };
+});
 
-app.use(router.routes()).use(router.allowedMethods())
+app.use(bodyParser());
+
+app.use(rootRouter.routes()).use(rootRouter.allowedMethods());
+app.use(authRouter.routes()).use(authRouter.allowedMethods());
+app.use(productsRouter.routes()).use(productsRouter.allowedMethods());
+app.use(cartsRouter.routes()).use(cartsRouter.allowedMethods());
+app.use(ordersRouter.routes()).use(ordersRouter.allowedMethods());
+app.use(reviewsRouter.routes()).use(reviewsRouter.allowedMethods());
+app.use(cartItemsRouter.routes()).use(cartItemsRouter.allowedMethods());
